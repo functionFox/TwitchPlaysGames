d2cb1c4870f5ca9d83f730a9bc909fbe0daedb40
functionFox <thefunctionfox@gmail.com>
2023-09-26 17:03:17 -0700

    Added a config template file. Rename to config.cs

---
+++ TwitchPlaysPeggle/configTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchPlaysGames
+{
+    public class config // Insert your details below, then rename this file to config.cs
+    {
+        public string oauth = "oauth:";
+        public string nick = "YourBotName";
+        public string channel = "YourChannel";
+        public string defaultGameName = "Peggle Deluxe";
+    }
+}
